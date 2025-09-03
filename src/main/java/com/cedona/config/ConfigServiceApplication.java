41db04ed7b99c2fe214d4a6f19d22e911bd75240
Surya Pranesh <spranesh@easypost.com>
2025-09-03 12:33:26 +0530

    Common Added

--- src/main/java/com/cedona/config/ConfigServiceApplication.java
+++ src/main/java/com/cedona/config/ConfigServiceApplication.java
@@ -3,7 +3,7 @@ package com.cedona.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.cedona.config", "com.oms.common"})
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
